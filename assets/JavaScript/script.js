a279f815746b22ece72fa93e7beb4b604c37cb40
unknown <blchilds55@gmail.com>
2020-10-11 19:21:17 -0500

    everything added except for logic

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,7 +2,7 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generate = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +10,36 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passwordLength = prompt("Enter a value between 8 and 128 for the password length: ");
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("The chosen password length value was out of range. Please choose a number between 8 and 128: ");
+  }
+
+  var lower = confirm("Confirm whether you want lowercases in the password.");
+  var upper = confirm("Confirm whether you want uppercases in the password.");
+  var numeric = confirm("Confirm whether you want numbers in the password.");
+  var special = confirm("Confirm whether you want special characters in the password.");
+
+  while (!lower && !upper && !numeric && !special) {
+    window.alert("You must have at least one choice included in your password.");
+
+    var lower = confirm("Confirm whether you want lowercases in the password.");
+    var upper = confirm("Confirm whether you want uppercases in the password.");
+    var numeric = confirm("Confirm whether you want numbers in the password.");
+    var special = confirm("Confirm whether you want special characters in the password.");
+  }
+
+  var characters_string = "";
+  var length = 0;
+  var password = "";
+  var add_from_string = "";
+
+  
+}
